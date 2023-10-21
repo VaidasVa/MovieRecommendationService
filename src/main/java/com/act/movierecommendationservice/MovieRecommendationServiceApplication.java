c562d8ce617a5bb6dee29da5b27b539e1958a648
vaidas.valikonis <valikonis.vaidas@gmail.com>
2023-10-21 19:20:05 +0300

    Initial commit

---
+++ src/main/java/com/act/movierecommendationservice/MovieRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.act.movierecommendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommendationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRecommendationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/act/movierecommendationservice/MovieRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.act.movierecommendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommendationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
